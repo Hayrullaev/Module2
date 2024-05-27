689ec6d5106be56270c2d15119ce46ca75492f26
Shohruh <shoh0554770876@gmail.com>
2024-05-27 12:45:42 +0300

    Update Homework4.py

--- Homework4.py
+++ Homework4.py
@@ -3,13 +3,13 @@ primes = []
 not_primes = []
 for num in numbers:
     is_prime = True
-for i in range(2, num // 2):
-    if num % i == 0:
-        is_prime = False
-    break
-if is_prime:
-    primes.append(num)
-else:
-    not_primes.append(num)
+    for i in range(2, num // 2):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
 print("Простые числа:", primes)
 print("Непростые числа:", not_primes)
